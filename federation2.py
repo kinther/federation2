8a87711d53818f2653f5af18ddec13e0e4c4716e
Kinther <xdelaranx@gmail.com>
2021-06-11 10:28:43 -0700

    v2.2 updates

--- federation2.py
+++ federation2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # Federation 2 Community Edition hauling scripts for planet owners
-# version 2.1.1 "Clandestine Clays" - hotfix
+# version 2.2 "Smuggled Semiconductors"
 
 # Imports
 import telnetlib  # used to do all things telnet
