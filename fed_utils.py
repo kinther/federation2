734a24d917a7d807066d065321836904c2243301
Kinther <xdelaranx@gmail.com>
2021-12-24 17:02:34 -0800

    Trying moving vars to utils as well

--- fed_utils.py
+++ fed_utils.py
@@ -13,6 +13,63 @@ from os import remove  # used to delete files
 from argparse import ArgumentParser  # used to pass user/password credentials
 from sys import exit  # used to exit script if criteria is met
 
+# argparse constants
+parser = ArgumentParser()
+parser.add_argument("--user", type=str, action="store", required=True)
+parser.add_argument("--password", type=str, action="store", required=True)
+parser.add_argument("--planet", type=str, action="store", required=True)
+parser.add_argument("--mode", type=str, action="store", required=True)
+args = parser.parse_args()
+
+# global constants
+ranks = ["Founder", "Engineer", "Mogul", "Technocrat", "Gengineer", "Magnate", "Plutocrat"]
+script_mode = (args.mode).lower()  # determines whether to focus on deficits or surpluses
+
+# Telnetlib variables
+host = "play.federation2.com"  # don't change this
+port = 30003  # don't change this
+timeout = 90  # maybe change this
+
+# telnetlib constants
+tn = Telnet(host, port, timeout=timeout)
+
+# Logging constants
+now = datetime.now()
+day = now.strftime("%a")
+hour = now.strftime("%H")
+minute = now.strftime("%M")
+LOG_FILENAME = (day + "-" + hour + minute + "-fed2.txt")
+basicConfig(filename=LOG_FILENAME, level=20)
+logger = getLogger()
+
+# Character constants
+HOME_PLANET = args.planet  # passed from player arguments
+DEFICIT = -75  # How much we consider a deficit
+SURPLUS = 18000  # How much we consider a surplus
+
+# Character variables
+balance = 0  # character's current balance, from output of score
+current_stamina = 0  # character's current stamina, from output of score
+stamina_min = 35  # lowest stamina level we want our character to fall to
+stamina_max = 0  # character's maximum stamina level, from output of score
+current_system = ""  # character is on this planet, from output of score
+current_planet = ""  # character is in this system, from output of score
+character_rank = ""  # character's rank, from output of score
+
+# Ship variables
+current_fuel = 0  # ship's current fuel level, from output of st
+fuel_min = 250  # lowest fuel level we want our ship to fall to
+fuel_max = 0  # ship's maximum stamina level, from output of st
+current_cargo = 0  # total cargo currently being hauled
+cargo_min = 0  # not sure if needed
+cargo_max = 0  # maximum tonnage ship can haul
+
+# Planet variables
+treasury = 0  # planet's current balance, from output of di planet
+exchange_dict = {}  # used to hold the exchange information
+deficits = []  # used to hold the current deficits list
+surpluses = []  # used to hold the current surpluses list
+
 # Global functions
 
 def login():
