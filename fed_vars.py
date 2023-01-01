1d483ebddf24c3d71d8ad589eac6987326f5c053
kinther <xdelaranx@gmail.com>
2023-01-01 08:15:20 -0800

    Add class strucutres based on previous vars

--- fed_vars.py
+++ fed_vars.py
@@ -13,6 +13,20 @@ current_planet = ""  # character is in this system, from output of score
 character_rank = ""  # character's rank, from output of score
 stamina_min = 35  # lowest stamina level we want our character to fall to
 
+# Character class structure
+
+class Player:
+    def __init__(self, score, balance, current_stamina, stamina_max,
+    current_system, current_planet, character_rank, stamina_min):
+        self.score = ""
+        self.balance = 0
+        self.current_stamina = 0
+        self.stamina_max = 0
+        self.current_system = ""
+        self.current_planet = ""
+        self.character_rank = ""
+        self.stamina_min = 35
+
 # Ship variables
 ship = ""  # output of the status report command held in memory
 current_fuel = 0  # ship's current fuel level, from output of st
@@ -22,6 +36,19 @@ current_cargo = 0  # total cargo currently being hauled
 cargo_min = 0  # not sure if needed
 cargo_max = 0  # maximum tonnage ship can haul
 
+# Ship class structure
+
+class Ship:
+    def __init__(self, ship, current_fuel, fuel_min, fuel_max,
+    current_cargo, cargo_min, cargo_max):
+        self.ship = ""
+        self.current_fuel = 0
+        self.fuel_min = 250
+        self.fuel_max = 0
+        self.current_cargo = 0
+        self.cargo_min = 0
+        self.cargo_max = 0
+
 # Planet variables
 planet = ""  # output of the di planet command held in memory
 exchange = ""  # output of the di exchange command held in memory
@@ -29,3 +56,15 @@ treasury = 0  # planet's current balance, from output of di planet
 exchange_dict = {}  # used to hold the exchange information
 deficits = []  # used to hold the current deficits list
 surpluses = []  # used to hold the current surpluses list
+
+# Planet class structure
+
+class Planet:
+    def __init__(self, planet, exchange, treasury, exchange_dict,
+    deficits, surpluses):
+        self.planet = ""
+        self.exchange = ""
+        self.treasury = 0
+        self.exchange_dict = {}
+        self.deficits = []
+        self.surpluses = []
