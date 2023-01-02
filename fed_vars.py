46209ca7e03365f86bea747589a88df203e683b1
kinther <patrick.t.walton@gmail.com>
2023-01-02 14:25:41 -0800

    Revert "New class structure"
    This reverts commit 9edc9bf74c663134a84a24ea073a62b433c6a895.

--- fed_vars.py
+++ fed_vars.py
@@ -16,14 +16,16 @@ stamina_min = 35  # lowest stamina level we want our character to fall to
 # Character class structure
 
 class Player:
-    score = ""
-    balance = 0
-    current_stamina = 0
-    stamina_max = 0
-    current_system = ""
-    current_planet = ""
-    character_rank = ""
-    stamina_min = 0
+    def __init__(self, score, balance, current_stamina, stamina_max,
+    current_system, current_planet, character_rank, stamina_min):
+        self.score = score
+        self.balance = balance
+        self.current_stamina = current_stamina
+        self.stamina_max = stamina_max
+        self.current_system = current_system
+        self.current_planet = current_planet
+        self.character_rank = character_rank
+        self.stamina_min = stamina_min
 
 # Ship variables
 ship = ""  # output of the status report command held in memory
@@ -36,12 +38,14 @@ cargo_max = 0  # maximum tonnage ship can haul
 # Ship class structure
 
 class Ship:
-    ship = ""
-    current_fuel = 0
-    fuel_min = 0
-    fuel_max = 0
-    current_cargo = 0
-    cargo_max = 0
+    def __init__(self, ship, current_fuel, fuel_min, fuel_max,
+    current_cargo, cargo_min, cargo_max):
+        self.ship = ship
+        self.current_fuel = current_fuel
+        self.fuel_min = fuel_min
+        self.fuel_max = fuel_max
+        self.current_cargo = current_cargo
+        self.cargo_max = cargo_max
 
 # Planet variables
 planet = ""  # output of the di planet command held in memory
@@ -54,9 +58,11 @@ surpluses = []  # used to hold the current surpluses list
 # Planet class structure
 
 class Planet:
-    planet = ""
-    exchange = ""
-    treasury = 0
-    exchange_dict = {}
-    deficits = []
-    surpluses = []
+    def __init__(self, planet, exchange, treasury, exchange_dict,
+    deficits, surpluses):
+        self.planet = planet
+        self.exchange = exchange
+        self.treasury = treasury
+        self.exchange_dict = exchange_dict
+        self.deficits = deficits
+        self.surpluses = surpluses
