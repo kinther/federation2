7b78c8307651e47a6804fc53e2b4650e7f9365f2
kinther <patrick.t.walton@gmail.com>
2023-01-03 19:44:06 -0800

    Problematic Proteins

--- federation2.py
+++ federation2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # Federation 2 Community Edition hauling scripts for planet owners
-# version 2.4 "Kurious Katydidics"
+# version 2.5 "Problematic Proteins"
 
 # Imports
 from fed_utils import *  # used to pull in all custom functions from fed_utils.py
