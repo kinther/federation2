f4967981715060b1216ddad5cc9966a2e5e95ebc
kinther <xdelaranx@gmail.com>
2023-01-12 08:41:30 -0800

    Version update

--- federation2.py
+++ federation2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # Federation 2 Community Edition hauling scripts for planet owners
-# version 2.5 "Problematic Proteins"
+# version 3.0 "Strictly Spices"
 
 # Imports
 from fed_utils import *  # used to pull in all custom functions from fed_utils.py
